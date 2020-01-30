6920f7874b83769b581ef2ac000f46c419b96ced
Jose <pjose14.jp38@yahoo.com>
2020-01-29 20:49:55 -0600

    set both timer and scores to zero

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { useState } from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+const [homeScore, setHomeScore] = useState(0);
+const [visitorScore, setVisitorScore] = useState(0);
+const [timer, setTimer] = useState(0);
+
 
   return (
     <div className="container">
@@ -15,12 +21,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{timer}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{visitorScore}</div>
           </div>
         </div>
         <BottomRow />
